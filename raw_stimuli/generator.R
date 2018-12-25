ae6a121f1d8c4664d2c0da42b65cadda3515f211
Pavel Logacev <pavel.logacev@gmail.com>
2018-12-25 12:20:21 +0300

    Updated experiment header.

--- raw_stimuli/generator.R
+++ raw_stimuli/generator.R
@@ -21,16 +21,16 @@ filler_ungram_sg %<>% .[1:20,]
 #experimental$adjunct %<>% gsub(" ", "_", .)
 
 #PL-SG-Vpl
-experimental$condition_a <- with(experimental, paste(dp_gen_pl, noun_poss, adjunct, mv_pl , sep = "\", \""))
+experimental$condition_a <- with(experimental, paste(dp_gen_pl, noun_poss, adjunct, mv_pl , sep = " "))
 
 #PL-SG-Vsg
-experimental$condition_b <- with(experimental, paste(dp_gen_pl, noun_poss, adjunct, mv_sg , sep = "\", \""))
+experimental$condition_b <- with(experimental, paste(dp_gen_pl, noun_poss, adjunct, mv_sg , sep = " "))
 
 #SG-SG-Vpl
-experimental$condition_c <- with(experimental, paste(dp_gen_sg, noun_poss, adjunct, mv_pl , sep = "\", \""))
+experimental$condition_c <- with(experimental, paste(dp_gen_sg, noun_poss, adjunct, mv_pl , sep = " "))
 
 #SG-SG-Vsg
-experimental$condition_d <- with(experimental, paste(dp_gen_sg, noun_poss, adjunct, mv_sg , sep = "\", \""))
+experimental$condition_d <- with(experimental, paste(dp_gen_sg, noun_poss, adjunct, mv_sg , sep = " "))
 
 experimental$item <- 1:40
 
@@ -46,18 +46,18 @@ View(stim_exp)
 #fillers
 fillers <- rbind(filler_gram_pl,filler_ungram_sg)
 
-fillers$filler <- with(fillers, paste(gen, poss, converb, obj , dist1, dist2, sep = "\", \""))
+fillers$filler <- with(fillers, paste(gen, poss, converb, obj , dist1, dist2, sep = " "))
 fillers$item <- 101:140
 stim_fill <- fillers %>% dplyr::select(filler, item)
 stim_fill %<>% tidyr::gather(condition, sentence, filler)
 stim_fill %<>% arrange(item, condition)
 stim_fill$ibex_sentence <- with(stim_fill, sprintf('[["%s", %d], "DashedSentence", {s: "%s"}]', condition, item, sentence))
 
-stim_exp_string <- paste(stim_exp$ibex_sentence, collapse = ",\n") %>% paste(",\n")
-filler_string <- paste(stim_fill$ibex_sentence, collapse = ",\n") #%>% paste(",\n")
-
+stim_exp_string <- paste(stim_exp$ibex_sentence, collapse = ",\n") 
+filler_string <- paste(stim_fill$ibex_sentence, collapse = ",\n")
+sentences_string <- paste(stim_exp_string, filler_string, sep = ",\n")
+  
 file.copy("stimuli_template_top", "stimuli.js", overwrite = T)
 file_out <- "stimuli.js" #file("stimuli.js", encoding = "UTF-8")
-cat(stim_exp_string, file = file_out, append = T )
-cat(filler_string, file = file_out, append = T )
+cat(sentences_string, file = file_out, append = T )
 cat( paste(readLines("stimuli_template_bottom", encoding = "utf-8"), collapse = "\n"), file = file_out, append = T)
